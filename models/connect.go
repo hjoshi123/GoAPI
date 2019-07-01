fec2d6656326a5d9d3554df8ed6ce0408930a503
hjoshi123 <joshi19981998@gmail.com>
2019-07-01 09:56:46 +0530

    [Update]:update in docker compose

--- models/connect.go
+++ models/connect.go
@@ -15,14 +15,23 @@ var db *gorm.DB
 func init() {
 
 	e := godotenv.Load()
+	var username string
+	var password string
+	var dbName string
+	var dbHost string
+
 	if e != nil {
 		fmt.Print(e)
+		username = os.Getenv("test")
+		password = os.Getenv("test143")
+		dbName = os.Getenv("contacts")
+		dbHost = os.Getenv("db")
+	} else {
+		username = os.Getenv("POSTGRES_USER")
+		password = os.Getenv("POSTGRES_PASSWORD")
+		dbName = os.Getenv("POSTGRES_DB")
+		dbHost = os.Getenv("db_host")
 	}
-
-	username := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PASSWORD")
-	dbName := os.Getenv("POSTGRES_DB")
-	dbHost := os.Getenv("db_host")
 
 	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password)
 	// fmt.Println(dbURI)
